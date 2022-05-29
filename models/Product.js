6055e781bdbe4c18a21e89a20b1fc9f651f56db0
mmikelic <mikelicmichael@gmail.com>
2022-05-29 12:04:02 -0400

    update models js

--- models/Product.js
+++ models/Product.js
@@ -10,14 +10,46 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
+  product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+  },
+  price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+          isDecimal: true
+      }
+  },
+  stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      Validate: {
+          isNumeric: true
+      }
+  },
+  category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+          model: 'category',
+          key: 'id'
+      }
   }
+}, {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'product',
+
+  },
 );
 
 module.exports = Product;
